fe7b3b69751a868240e10985cf4d3af68ed0c4d9
John <cooljohne@gmail.com>
2026-04-23 17:04:42 +0100

    deployment

--- app/(shared-routes)/page.tsx
+++ app/(shared-routes)/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-2">
       <h1 className="text-4xl font-bold">This is a dummy home page</h1>
       <p className="mt-4 text-lg">This is just a simple placeholder page. The site is actually just to demonstrate creating forms with React Hook Form and Zod. Using shadcn/ui and Tailwind CSS also.</p>
     </main>
